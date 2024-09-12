14110bd3e1cd6b894ec1c2b978031a1880f23163
venugopal5501 <venugopal14082002@gmail.com>
2024-09-12 18:33:23 +0530

    first commit

---
+++ 1.reverse.cs
@@ -0,0 +1,16 @@
+using System;
+class reversed
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter Number : ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        int[] array = new int[a];
+        for (int i = 0; i < a; i++)
+        {
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        int app=
+        Console.Write(array);
+    }
+}
